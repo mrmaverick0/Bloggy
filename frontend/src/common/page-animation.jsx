866004594a88a04e90b6721017d24d35a6ac3b46
Mohd Umar <ua1411978@gmail.com>
2024-04-15 23:56:40 +0530

    Adding framer motion animation in user auth form toggle between singnin and signup page

---
+++ frontend/src/common/page-animation.jsx
@@ -0,0 +1,22 @@
+import { motion } from "framer-motion"
+
+ const AnimationWrapper = (
+  { children,className, keyValue, initial = { opacity: 0 },animate = { opacity: 1 },
+  transition = { duration: 1 } },
+  
+) => {
+  return (
+    <motion.div
+      key={keyValue}
+      initial={initial}
+      animate={animate}
+      transition={transition}
+      className={className}
+    >
+      {children}
+    </motion.div>
+  );
+};
+export default AnimationWrapper
+
+
